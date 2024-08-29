e570264d92bf7379c9eff247317aa81135001e6a
Maicon Cardoso <maiconscardoso@hotmail.com>
2024-08-29 07:01:27 -0300

    Connecting mongodb and service e repository

--- src/main/java/com/maicon/devsuperior/workshopmongo/resources/UserResource.java
+++ src/main/java/com/maicon/devsuperior/workshopmongo/resources/UserResource.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,17 +12,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.maicon.devsuperior.workshopmongo.domain.User;
+import com.maicon.devsuperior.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+    @Autowired
+    private UserService service;
+
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<List<User>> findAll() {
-        User maria = new User("1", "Maria Brown", "maria@gmail.com");
-        User alex = new User("2", "Alex Green", "alex@hotmail.com");
-        List<User> list = new ArrayList<>();
-        list.addAll(Arrays.asList(maria, alex));
+        List<User> list = service.findAll();        
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 }
